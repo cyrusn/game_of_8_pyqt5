9b600abcc0325b99fd3216fbc2ec223c0bc56f8e
cyrusn <cyrusncy@gmail.com>
2018-06-04 19:49:49 +0800

    cleaner code

--- gameBoard.py
+++ gameBoard.py
@@ -6,19 +6,19 @@ class Game():
 
     def __init__(self):
         super().__init__()
-        # cursor_location is the location of blank tile
-        self.cursor_location = [2, 2]
+        # cursor_loc is the location of blank tile
         self.default = [['8', '7', '6'],
                         ['5', '4', '3'],
                         ['2', '1', ' ']]
 
         self.board = deepcopy(self.default)
-
-        self.goal = [['1', '2', '3'],
-                     ['4', '5', '6'],
-                     ['7', '8', ' ']]
         self.counter = 0
         self.win = False
+
+        self.__cursor_loc = [2, 2]
+        self.__goal = [['1', '2', '3'],
+                       ['4', '5', '6'],
+                       ['7', '8', ' ']]
 
     # if the given position can move (the cursor tile can replace it), it
     # moves.
@@ -28,62 +28,57 @@ class Game():
             return
 
         (x, y) = position
-        (cx, cy) = self.cursor_location
+        # store cursor loc, using tuple because tuple is immutable
+        (cx, cy) = self.__cursor_loc
 
-        # check the cursor location and move
+        # check the cursor location and update
         if x == cx and y + 1 == cy:
-            self.move_left()
+            self.__update_cursor_loc('left')
         if x == cx and y - 1 == cy:
-            self.move_right()
+            self.__update_cursor_loc('right')
         if x + 1 == cx and y == cy:
-            self.move_up()
+            self.__update_cursor_loc('up')
         if x - 1 == cx and y == cy:
-            self.move_down()
+            self.__update_cursor_loc('down')
 
-    def move_up(self):
-        if self.cursor_location[0] > 0:
-            old = self.cursor_location[:]
-            self.cursor_location[0] -= 1
-            self.move_cursor(old)
+        # update the board by with old cursor_loc as argument.
+        # the updated cursor_loc can be retrieved in __update_board() by
+        # self.__cursor_loc
+        self.__update_board((cx, cy))
 
-    def move_down(self):
-        if self.cursor_location[0] < 2:
-            old = self.cursor_location[:]
-            self.cursor_location[0] += 1
-            self.move_cursor(old)
+    def __update_cursor_loc(self, direction):
+        if direction == 'up':
+            self.__cursor_loc[0] -= 1
 
-    def move_left(self):
-        if self.cursor_location[1] > 0:
-            old = self.cursor_location[:]
-            self.cursor_location[1] -= 1
-            self.move_cursor(old)
+        if direction == 'down':
+            self.__cursor_loc[0] += 1
 
-    def move_right(self):
-        if self.cursor_location[1] < 2:
-            old = self.cursor_location[:]
-            self.cursor_location[1] += 1
-            self.move_cursor(old)
+        if direction == 'left':
+            self.__cursor_loc[1] -= 1
 
-    def move_cursor(self, old):
-        self.add1()
+        if direction == 'right':
+            self.__cursor_loc[1] += 1
+
+    def __update_board(self, old):
+        self.__counter_increment()
 
         (x, y) = old
-        (nx,  ny) = self.cursor_location
+        (nx,  ny) = self.__cursor_loc
 
         dummy = self.board[x][y]
         self.board[x][y] = self.board[nx][ny]
         self.board[nx][ny] = dummy
-        self.check_win()
+        self.__check_win()
 
-    def check_win(self):
-        if self.board == self.goal:
+    def __check_win(self):
+        if self.board == self.__goal:
             self.win = True
 
-    def add1(self):
+    def __counter_increment(self):
         self.counter += 1
 
     def reset(self):
         self.counter = 0
         self.win = False
-        self.cursor_location = [2, 2]
+        self.__cursor_loc = [2, 2]
         self.board = deepcopy(self.default)
